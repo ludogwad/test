d799326147c906948b02cccef08be1c62dbd6440
Ludovick <Ludovick@192.168.1.14>
2013-12-25 12:23:33 +0100

    premier fichier

---
+++ CppApplication_5/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Ludovick
+ *
+ * Created on 25 décembre 2013, 12:22
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
